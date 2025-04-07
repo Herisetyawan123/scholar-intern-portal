e91e7e49e34a2823b7247b6d6117e8179a659c98
Heri Setyawan <herisetyawan233@gmail.com>
2025-04-07 11:00:23 +0700

    duh lupa

--- src/components/index/featured-scholarship-section.jsx
+++ src/components/index/featured-scholarship-section.jsx
@@ -2,6 +2,7 @@ import { CalendarIcon, ArrowUpRightIcon } from "@heroicons/react/24/outline";
 import ScholarCard from "./ui/scholar-card";
 import { useContext } from "react";
 import HomeContext from "../../state-management/home/home-context";
+import { Link } from "react-router-dom";
 
 export default function FeaturedScholarshipsSection() {
   const data = useContext(HomeContext);
@@ -14,9 +15,9 @@ export default function FeaturedScholarshipsSection() {
             <h2 className="text-3xl font-bold text-gray-800">Featured Scholarships</h2>
             <p className="text-gray-500">Explore the top scholarship opportunities</p>
           </div>
-          <a href="#" className="text-blue-600 hover:underline flex items-center gap-1">
+          <Link to={'/scholarships'} className="text-blue-600 hover:underline flex items-center gap-1">
             View All <ArrowUpRightIcon className="w-4 h-4" />
-          </a>
+          </Link>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {data.scholarships.map((item, index) => (
